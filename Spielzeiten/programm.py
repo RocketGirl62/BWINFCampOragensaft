d7868c9d63fe697f6510c8138910202edebd9761
Isabel Reinema <isabel.reinema@outlook.de>
2022-01-29 12:39:56 +0100

    FGD

--- Spielzeiten/programm.py
+++ Spielzeiten/programm.py
@@ -22,7 +22,6 @@ n = int(allezeilen[0])
 #tlist = []
 lst = []
 for i in range(1, n+1):
-    lst = []
     allezeilen[i] = allezeilen[i].rstrip("\n")
     print(allezeilen[i])
 
@@ -34,7 +33,7 @@ for i in range(1, n+1):
 
 lst.sort(key=lambda x: x[1])
 print(lst)
-
+#Strings müssen noch Integern, sortieren funktioniert
 
 
 wert1 = []
