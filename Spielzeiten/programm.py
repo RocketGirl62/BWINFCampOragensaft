5097caab00daf78d51c62db5afb9943af054d53f
Isabel Reinema <isabel.reinema@outlook.de>
2022-01-29 13:33:08 +0100

    af

--- Spielzeiten/programm.py
+++ Spielzeiten/programm.py
@@ -19,21 +19,21 @@ except:
     sys.exit(0)
 
 n = int(allezeilen[0])
-#tlist = []
+
 lst = []
 for i in range(1, n+1):
     allezeilen[i] = allezeilen[i].rstrip("\n")
     print(allezeilen[i])
-
-    lst.append(allezeilen[i].split(" "))
-    #tlist = tlist + lst
-
+    tmp = allezeilen[i].split(" ")
+    tmp[0]=int(tmp[0])
+    tmp[1]=int(tmp[1])
+    lst.append(tmp)
     print(lst)
-    #print(tlist)
+
 
 lst.sort(key=lambda x: x[1])
 print(lst)
-#Strings müssen noch Integern, sortieren funktioniert
+
 
 
 wert1 = []
