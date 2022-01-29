53db669ef4d3ad3d5812d0c6ae6571019ffafc09
Isabel Reinema <isabel.reinema@outlook.de>
2022-01-29 12:34:25 +0100

    sdaf

--- Spielzeiten/programm.py
+++ Spielzeiten/programm.py
@@ -19,21 +19,21 @@ except:
     sys.exit(0)
 
 n = int(allezeilen[0])
-tlist = []
+#tlist = []
 lst = []
 for i in range(1, n+1):
     lst = []
     allezeilen[i] = allezeilen[i].rstrip("\n")
     print(allezeilen[i])
 
-    lst = allezeilen[i].split(" ")
-    tlist = tlist + lst
+    lst.append(allezeilen[i].split(" "))
+    #tlist = tlist + lst
 
     print(lst)
-    print(tlist)
+    #print(tlist)
 
-tlist.sort(key=lambda x: x[1])
-print(tlist)
+lst.sort(key=lambda x: x[1])
+print(lst)
 
 
 
