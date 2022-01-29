ca554885741daa530d2901a985bf61c0d87b1c70
Isabel Reinema <isabel.reinema@outlook.de>
2022-01-29 12:33:29 +0100

    asdg

--- Spielzeiten/programm.py
+++ Spielzeiten/programm.py
@@ -13,22 +13,27 @@ allezeilen = e.readlines()
 e.close()
 
 try:
-    d = open("Spielzeiten_small.out","w")
+    d = open("ausgabe.txt","w")
 except:
     print("error")
     sys.exit(0)
 
 n = int(allezeilen[0])
 tlist = []
+lst = []
 for i in range(1, n+1):
+    lst = []
     allezeilen[i] = allezeilen[i].rstrip("\n")
     print(allezeilen[i])
 
-    list = allezeilen[i].split(" ")
+    lst = allezeilen[i].split(" ")
+    tlist = tlist + lst
 
+    print(lst)
+    print(tlist)
 
-    print(list)
-
+tlist.sort(key=lambda x: x[1])
+print(tlist)
 
 
 
