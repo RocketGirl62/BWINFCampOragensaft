1fe331e64621ba6f1b30a9442cbb732ec747b724
Soly4G <87171369+Soly4G@users.noreply.github.com>
2022-01-29 12:13:44 +0100

    Bahnhof

--- Spielzeiten/programm.py
+++ Spielzeiten/programm.py
@@ -1,8 +1,7 @@
 import sys
-
 # Zugriffsversuch
 try:
-    e = open("SchereSteinPapier_big.in.txt")
+    e = open("Spielzeiten_small.in.txt")
 except:
     print("Dateizugriff nicht erfolgreich")
     sys.exit(0)
@@ -14,13 +13,24 @@ allezeilen = e.readlines()
 e.close()
 
 try:
-    d = open("ausgabe2.txt","w")
+    d = open("Spielzeiten_small.out","w")
 except:
     print("error")
     sys.exit(0)
 
 n = int(allezeilen[0])
-print("hello World")
+tlist = []
+for i in range(1, n+1):
+    allezeilen[i] = allezeilen[i].rstrip("\n")
+    print(allezeilen[i])
+
+    list = allezeilen[i].split(" ")
+
+
+    print(list)
+
+
+
 
 wert1 = []
 wert2 = []
