203b20610b8154b1c0c8904d413b1f226fff322b
Isabel Reinema <isabel.reinema@outlook.de>
2022-01-29 15:55:31 +0100

    fdgh

--- Spielzeiten/programm - Kopie.py
+++ Spielzeiten/programm - Kopie.py
@@ -1,7 +1,7 @@
 import sys
 # Zugriffsversuch
 try:
-    e = open("Spielzeiten_small.in.txt")
+    e = open("Spielzeiten_big.in.txt")
 except:
     print("Dateizugriff nicht erfolgreich")
     sys.exit(0)
@@ -23,18 +23,18 @@ n = int(allezeilen[0])
 lst = []
 for i in range(1, n+1):
     allezeilen[i] = allezeilen[i].rstrip("\n")
-    print(allezeilen[i])
+
     tmp = allezeilen[i].split(" ")
     tmp[0]=int(tmp[0])
     tmp[1]=int(tmp[1])
     lst.append(tmp)
-    print(lst)
+
 
 
 
 
 lst.sort(key=lambda x: x[0])
-print(lst)
+
 
 
 
